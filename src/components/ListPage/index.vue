048c13b03e59c017d09623b81b588952f47df90e
weiyurun <weiyurun@xdf.cn>
2023-09-11 10:03:40 +0800

    wei

--- src/components/ListPage/index.vue
+++ src/components/ListPage/index.vue
@@ -629,8 +629,8 @@ async function searchReset() {
   for (let p in params.value) {
     let findItem = searchItems.value.find((i) => i.propName === p);
     if (findItem) {
-      if (!findItem?.readOnly?.realValue && !findItem?.hidden?.realValue) {
-        params.value[p] = findItem?.defaultValue?.realValue ?? null;
+      if (!findItem?.readOnly?.xnsk_admin_ui_realValue && !findItem?.hidden?.xnsk_admin_ui_realValue) {
+        params.value[p] = findItem?.defaultValue?.xnsk_admin_ui_realValue ?? null;
       }
     }
   }
